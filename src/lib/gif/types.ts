a97b981cb73f5c3ad1d4d5102d17d24808acf2b2
Vireterr <160679166+Vireterr@users.noreply.github.com>
2026-08-31 07:58:32 +0300

    Update types.ts

--- src/lib/gif/types.ts
+++ src/lib/gif/types.ts
@@ -28,6 +28,9 @@ export interface StyleProfile {
   color: ColorProfile;
   motionProfile: MotionProfile;
   textureProfile: TextureProfile;
+  mode?: string;
+  params?: any;
+  effects?: any;
 }
 
 export interface StyleParams {
